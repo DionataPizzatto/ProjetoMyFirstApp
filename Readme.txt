4a829d69f0703c00388864e8eb96ba3f8d65065b
dionatapizzatto <dionata_pizzatto@hotmail.com>
2014-04-03 18:20:42 -0300

    Starting Another Activity

--- Readme.txt
+++ Readme.txt
@@ -1,3 +1,4 @@
 a - Creating an Android Project
 b- Running your App
 c-Building a Simple User Interface
+d- Starting Another Activity
--- src/com/example/myfirstapp/DisplayMessageActivity.java
+++ src/com/example/myfirstapp/DisplayMessageActivity.java
@@ -10,28 +10,33 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
+import android.widget.TextView;
 import android.os.Build;
 
-public class MainActivity extends ActionBarActivity {
-	 public  final  static  String EXTRA_MESSAGE =  "com.example.myfirstapp.MESSAGE" ; 
-	    
+public class DisplayMessageActivity extends ActionBarActivity {
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
 
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
+	    // Get the message from the intent
+	    Intent intent = getIntent();
+	    String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+	    // Create the text view
+	    TextView textView = new TextView(this);
+	    textView.setTextSize(40);
+	    textView.setText(message);
+
+	    // Set the text view as the activity layout
+	    setContentView(textView); 
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.display_message, menu);
 		return true;
 	}
 
@@ -58,20 +63,10 @@ public class MainActivity extends ActionBarActivity {
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
 				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
+			View rootView = inflater.inflate(R.layout.fragment_display_message,
+					container, false);
 			return rootView;
 		}
-	}
-	
-	public void sendMessage(View view) {
-	    // Do something in response to button
-		Intent intent = new Intent(this, DisplayMessageActivity.class);
-	    EditText editText = (EditText) findViewById(R.id.edit_message);
-	    String message = editText.getText().toString();
-	    intent.putExtra(EXTRA_MESSAGE, message);
-	    startActivity(intent);
-		
 	}
 
 }
